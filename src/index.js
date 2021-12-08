ef78b60453c85b3f0cd6428273586bfa1a9a7c44
DivyanshArya23 <divyansharya20@gmail.com>
2021-12-08 22:34:30 +0530

    mongoose connection done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const app = require("express")();
+const mongoose = require("mongoose");
+
+const port = process.env.PORT;
+const mongoURI = `mongodb+srv://admin:${process.env.DB_PASS}@trackapp.suypx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoURI);
+mongoose.connection.on("connected", () => {
+  console.log("connected to instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to mongoose", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi There!");
