b8009d043325f230d7395425e4164c2f4b653a0a
DivyanshArya23 <divyansharya20@gmail.com>
2021-12-08 23:20:19 +0530

    Video 183

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,9 +7,13 @@ const router = express.Router();
 
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
-  const user = new User({ email, password });
-  await user.save();
-  res.send("You made a post Request");
+  try {
+    const user = new User({ email, password });
+    await user.save();
+    res.send("You made a post Request");
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
 });
 
 module.exports = router;
